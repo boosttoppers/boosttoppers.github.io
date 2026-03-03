0ba234f6354d797274146884d8bb72ab66b85b50
boosttoppers <collaboration.vikas@gmail.com>
2026-03-03 23:25:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
 let slideIndex = 0;
+
 showSlides();
 
-function showSlides() {
+function showSlides(){
 
 let slides = document.getElementsByClassName("slide");
 let dots = document.getElementsByClassName("dot");
 
-for (let i = 0; i < slides.length; i++) {
-slides[i].style.display = "none";
+for(let i=0;i<slides.length;i++){
+slides[i].style.display="none";
 }
 
 slideIndex++;
 
-if (slideIndex > slides.length) {
-slideIndex = 1;
+if(slideIndex>slides.length){
+slideIndex=1;
 }
 
-for (let i = 0; i < dots.length; i++) {
-dots[i].className =
-dots[i].className.replace(" active","");
+slides[slideIndex-1].style.display="block";
+
+for(let i=0;i<dots.length;i++){
+dots[i].classList.remove("active");
 }
 
-slides[slideIndex-1].style.display = "block";
-dots[slideIndex-1].className += " active";
+dots[slideIndex-1].classList.add("active");
 
-setTimeout(showSlides, 3000);
+setTimeout(showSlides,3000);
 }
 function openLogin(){
 document.getElementById("loginPopup").style.display="flex";
