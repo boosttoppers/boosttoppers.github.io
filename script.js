588c31c30063a569f8579f42e50e49ab0cab8934
boosttoppers <collaboration.vikas@gmail.com>
2026-03-05 19:53:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -37,3 +37,69 @@ document.getElementById("loginPopup").style.display="flex";
 function closeLogin(){
 document.getElementById("loginPopup").style.display="none";
 }
+let slideIndex = 0;
+showSlides();
+
+function showSlides(){
+
+let slides = document.getElementsByClassName("slides");
+let dots = document.getElementsByClassName("dot");
+
+for(let i=0;i<slides.length;i++){
+slides[i].style.display="none";
+}
+
+slideIndex++;
+
+if(slideIndex>slides.length){
+slideIndex=1;
+}
+
+slides[slideIndex-1].style.display="block";
+
+setTimeout(showSlides,3000);
+}
+
+function openLogin(){
+document.getElementById("loginPopup").style.display="flex";
+}
+
+function closeLogin(){
+document.getElementById("loginPopup").style.display="none";
+}
+
+
+/* ===== OTP LOGIN SYSTEM ===== */
+
+let generatedOTP = "";
+
+function sendOTP(){
+
+let mobile = document.getElementById("mobile").value;
+
+if(mobile.length != 10){
+alert("Enter valid mobile number");
+return;
+}
+
+generatedOTP = Math.floor(1000 + Math.random()*9000);
+
+alert("Your OTP is: " + generatedOTP);
+}
+
+function verifyOTP(){
+
+let otp = document.getElementById("otp").value;
+
+if(otp == generatedOTP){
+
+alert("Login Successful");
+closeLogin();
+
+}else{
+
+alert("Wrong OTP");
+
+}
+
+}
