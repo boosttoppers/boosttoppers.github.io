073917eacb7aa09bfdc1f28c298174ea8da2ec45
boosttoppers <collaboration.vikas@gmail.com>
2026-03-13 20:48:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -2,54 +2,69 @@
 
 let slideIndex = 0;
 
-window.onload = function () {
-  showSlides();
-  renderRecaptcha();
+window.onload = function(){
+showSlides();
+renderRecaptcha();
 };
 
-function showSlides() {
+function showSlides(){
 
-  let slides = document.getElementsByClassName("slides");
+let slides = document.getElementsByClassName("slides");
 
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
+for(let i=0;i<slides.length;i++){
+slides[i].style.display="none";
+}
 
-  slideIndex++;
+slideIndex++;
 
-  if (slideIndex > slides.length) {
-    slideIndex = 1;
-  }
+if(slideIndex > slides.length){
+slideIndex = 1;
+}
 
-  slides[slideIndex - 1].style.display = "block";
+slides[slideIndex-1].style.display="block";
 
-  setTimeout(showSlides, 3000);
+setTimeout(showSlides,3000);
 }
 
 
 /* ===== LOGIN POPUP ===== */
 
-function openLogin() {
-  document.getElementById("loginPopup").style.display = "flex";
+function openLogin(){
+document.getElementById("loginPopup").style.display="flex";
 }
 
-function closeLogin() {
-  document.getElementById("loginPopup").style.display = "none";
+function closeLogin(){
+document.getElementById("loginPopup").style.display="none";
 }
+
+
+/* ===== FIREBASE CONFIG ===== */
+
+var firebaseConfig = {
+apiKey: "NEW_API_KEY",
+authDomain: "boost-toppers.firebaseapp.com",
+projectId: "boost-toppers",
+storageBucket: "boost-toppers.firebasestorage.app",
+messagingSenderId: "258617068875",
+appId: "1:258617068875:web:XXXX"
+};
+
+firebase.initializeApp(firebaseConfig);
 
 
 /* ===== FIREBASE RECAPTCHA ===== */
 
-function renderRecaptcha() {
+function renderRecaptcha(){
 
-  window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
-    'recaptcha-container',
-    {
-      size: 'normal'
-    }
-  );
+window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
+'recaptcha-container',
+{
+size:'normal'
+}
+);
 
-  recaptchaVerifier.render();
+recaptchaVerifier.render();
+
 }
 
 
@@ -57,7 +72,12 @@ function renderRecaptcha() {
 
 function sendOTP(){
 
-let mobile = "8081924678";
+let mobile = document.getElementById("mobile").value;
+
+if(mobile.length != 10){
+alert("Enter valid mobile number");
+return;
+}
 
 let number = "+91" + mobile;
 
@@ -78,60 +98,47 @@ alert(error.message);
 });
 
 }
+
+
 /* ===== VERIFY OTP ===== */
 
-function verifyOTP() {
+function verifyOTP(){
 
-  let code = "";
+let code = "";
 
-  let boxes = document.querySelectorAll(".otp-box");
+document.querySelectorAll(".otp-box").forEach((box)=>{
+code += box.value;
+});
 
-  boxes.forEach((box) => {
-    code += box.value;
-  });
+if(code.length < 6){
+alert("Enter complete OTP");
+return;
+}
 
-  if (code.length < 6) {
-    alert("Enter complete OTP");
-    return;
-  }
+confirmationResult.confirm(code)
 
-  confirmationResult.confirm(code)
+.then(function(){
 
-    .then(function () {
+alert("Login Successful");
+closeLogin();
 
-      alert("Login Successful");
+})
 
-      closeLogin();
+.catch(function(){
 
-    })
+alert("Wrong OTP");
 
-    .catch(function () {
-
-      alert("Wrong OTP");
-
-    });
+});
 
 }
-var firebaseConfig = {
-apiKey: "NEW_API_KEY",
-authDomain: "boost-toppers.firebaseapp.com",
-projectId: "boost-toppers",
-storageBucket: "boost-toppers.firebasestorage.app",
-messagingSenderId: "258617068875",
-appId: "1:258617068875:web:XXXX"
-};
-window.recaptchaVerifier = new firebase.auth.RecaptchaVerifier(
-'recaptcha-container',
-{
-size: 'normal'
-}
-);
 
-recaptchaVerifier.render();
+
+/* ===== SIDE MENU ===== */
+
 function openMenu(){
 document.getElementById("sideMenu").style.left="0";
 }
 
 function closeMenu(){
 document.getElementById("sideMenu").style.left="-100%";
-}
+       }      
